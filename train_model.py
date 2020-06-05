acb374fec408765230b3020add58cf83a013a002
Spooneliate <i_demil@live.co.uk>
2020-06-05 17:20:06 +0100

    Added matlab plotting for evaluating network performance.

--- train_model.py
+++ train_model.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import numpy as np
+import pickle
 
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import Adam
@@ -9,8 +11,10 @@ from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCh
 
 from tensorflow.keras.applications.mobilenet import MobileNet, preprocess_input
 
+import matplotlib.pyplot as plt
+
 # This is the directory where the trained model will be saved
-trained_model_dir = 'data/trained'
+trained_model_dir = 'models/trained/mobilenet_retrained.h5'
 
 train_dir = 'data/train'
 valid_dir = 'data/valid'
@@ -18,8 +22,8 @@ valid_dir = 'data/valid'
 #Default model is MobileNet
 model_path = 'models\mobilenet0.25_no_top.h5' 
 
-batch_size_train = 8
-batch_size_valid = 8
+batch_size_train = 10
+batch_size_valid = 10
 
 # Load the training and validation sets into Dataset objects
 imageDataGenerator = ImageDataGenerator(preprocessing_function=preprocess_input)
@@ -44,6 +48,7 @@ x = Dropout(0.25)(x)
 predictions = Dense(7, activation='softmax')(x)
 model = Model(inputs=mobileNet.input, outputs=predictions)
 
+
 # Set all the layers except the last as trainable: This can be changed in the future
 for layer in model.layers[:-6]:
 	layer.trainable = False
@@ -53,6 +58,39 @@ model.summary()
 # Compile the model
 model.compile(Adam(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
 
-train_log = model.fit(train_data, steps_per_epoch=np.ceil(6000/8), validation_data=valid_data, validation_steps=np.ceil(2000/8), epochs=25, verbose=1)
+hist = model.fit(train_data, steps_per_epoch=np.ceil(6000/8), validation_data=valid_data, validation_steps=np.ceil(2000/8), epochs=35, verbose=1)
 
-model.save(trained_model_dir + '/mobileNet_0.25_finalLayerTrained.h5')
+# Save trained model
+model.save(trained_model_dir)
+
+# Save history for later use
+with open('/trainHistoryDictMobileNetpt1', 'wb') as file_pi:
+        pickle.dump(hist.history, file_pi)
+
+train_loss = hist.history['loss']
+val_loss = hist.history['val_loss']
+train_acc = hist.history['acc']
+val_acc = hist.history['val_acc']
+epochs = range(35)
+
+plt.figure(1, figsize=(7,5))
+plt.plot(epochs, train_loss, 'go')
+plt.plot(epochs, val_loss)
+plt.xlabel('No. of epochs')
+plt.ylabel('Loss')
+plt.title('validation and training loss')
+plt.grid(True)
+plt.legend(['Training loss', 'Validation loss'])
+plt.style.use(['classic'])
+
+plt.figure(2, figsize=(7,5))
+plt.plot(epochs, train_acc, 'go')
+plt.plot(epochs, val_acc)
+plt.xlabel('No. of epochs')
+plt.ylabel('Accuracy')
+plt.title('validation and training accuracy')
+plt.grid(True)
+plt.legend(['Training accuracy', 'Validation accuracy'])
+plt.style.use(['classic'])
+
+plt.show()
